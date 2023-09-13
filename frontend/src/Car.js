6efd210d7493e350427182bb72821b7d3f04ec01
Robert Regalado <rjregalado1979@gmail.com>
2023-09-13 13:06:23 +0800

    modified car.js

--- frontend/src/Car.js
+++ frontend/src/Car.js
@@ -1,6 +1,12 @@
 
 function Car(props) {
-  return <h2>I am a {props.color} Car!</h2>;
+  return (
+  <>
+  <h2>I am a {props.color} Car!</h2>;
+  <h2>I am a {props.color} Car!</h2>;
+  
+  </>
+  );
 }
 
 export default Car;
